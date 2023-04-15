df616a701d9acc84b31d1512e6ffb26cf3423723
Janus Garzon <janus.garzon@usjr.edu.ph>
2023-04-15 14:20:02 +0800

    movement jump crouch

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     private CharacterController2D m_PlayerController;
-    private Vector2 playerMovement;
+
+    [SerializeField]
+    private float _speed = 5;
+    private float m_Direction;
+    private bool m_HasJumped = false;
+    private bool m_HasCrouched = false;
 
     private void Awake()
     {
@@ -15,16 +19,31 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
+        m_Direction = Input.GetAxis("Horizontal") * _speed;
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            m_HasJumped = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            m_HasCrouched = true;
+        }
+        else if (Input.GetKeyUp(KeyCode.DownArrow))
+        {
+            m_HasCrouched = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
         MovePlayer();
     }
 
     void MovePlayer()
     {
-        
-    }
-
-    void OnMove(InputValue inputValue)
-    {
-        playerMovement = inputValue.Get<Vector2>();
+        m_PlayerController.Move(m_Direction * Time.fixedDeltaTime, m_HasCrouched, m_HasJumped);
+        m_HasJumped = false;
     }
 }
