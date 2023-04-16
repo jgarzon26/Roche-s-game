0b08a1807d78d4e634d5979bbfd8a6b800417c84
Josh Mats <otwacraft@gmail.com>
2023-04-16 08:13:27 +0800

    flight & mana regen
    - changed mana to float
    - added passive mana regen
    - added flight using mana
    - added "Fly" input thing
    *flight is toggled by a public bool canFly
    
    -also tweaked some rigidbody and movements stats idk how they'd be tho

--- Assets/Scripts/Player/CharacterController2D.cs
+++ Assets/Scripts/Player/CharacterController2D.cs
@@ -3,7 +3,8 @@ using UnityEngine.Events;
 
 public class CharacterController2D : MonoBehaviour
 {
-	[SerializeField] private float m_JumpForce = 400f;							// Amount of force added when the player jumps.
+	[SerializeField] private float m_JumpForce = 400f;                          // Amount of force added when the player jumps.
+	[SerializeField] private float m_LiftForce = 300f;
 	[Range(0, 1)] [SerializeField] private float m_CrouchSpeed = .36f;			// Amount of maxSpeed applied to crouching movement. 1 = 100%
 	[Range(0, .3f)] [SerializeField] private float m_MovementSmoothing = .05f;	// How much to smooth out the movement
 	[SerializeField] private bool m_AirControl = false;							// Whether or not a player can steer while jumping;
@@ -128,10 +129,14 @@ public class CharacterController2D : MonoBehaviour
 		{
 			// Add a vertical force to the player.
 			m_Grounded = false;
-			m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce));
+			m_Rigidbody2D.AddForce(new Vector2(0f, m_JumpForce), ForceMode2D.Impulse);
 		}
 	}
 
+	public void Fly()
+	{
+        m_Rigidbody2D.AddForce(new Vector2(0f, m_LiftForce));
+    }
 
 	private void Flip()
 	{
